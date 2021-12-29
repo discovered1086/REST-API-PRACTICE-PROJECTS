cc1a1993e2e9c87b91383681b36d0af59c038cbb
Kingshuk Mukherjee <sly.mania42@gmail.com>
2021-12-28 19:58:01 -0600

    file upload and download API added

---
+++ file-upload-download-api/src/main/java/com/kingshuk/restapi/fileuploaddownloadapi/FileUploadDownloadApiApplication.java
@@ -0,0 +1,13 @@
+package com.kingshuk.restapi.fileuploaddownloadapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadDownloadApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileUploadDownloadApiApplication.class, args);
+    }
+
+}
