073c294134fa82fe8863676a9307943fa7537542
Kingshuk Mukherjee <sly.mania42@gmail.com>
2021-12-30 11:00:22 -0600

    Test cases added for writing clients for upload and download APIs

--- file-upload-download-api/src/main/java/com/kingshuk/restapi/fileuploaddownloadapi/FileUploadDownloadApiApplication.java
+++ file-upload-download-api/src/main/java/com/kingshuk/restapi/fileuploaddownloadapi/FileUploadDownloadApiApplication.java
@@ -2,12 +2,19 @@ package com.kingshuk.restapi.fileuploaddownloadapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class FileUploadDownloadApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FileUploadDownloadApiApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 
 }
--- file-upload-download-api/src/test/java/com/kingshuk/restapi/fileuploaddownloadapi/FileUploadDownloadApiApplicationTests.java
+++ file-upload-download-api/src/test/java/com/kingshuk/restapi/fileuploaddownloadapi/FileUploadDownloadApiApplicationTests.java
@@ -1,13 +1,61 @@
 package com.kingshuk.restapi.fileuploaddownloadapi;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.*;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collections;
 
 @SpringBootTest
 class FileUploadDownloadApiApplicationTests {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FileUploadDownloadApiApplicationTests.class);
+
+    @Value("${fileLocation}")
+    private String directory;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @Test
-    void contextLoads() {
+    void uploadFile() throws IOException {
+        //We first specify the form data as multi-part form data
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        MultiValueMap<String, Object> valueMap = new LinkedMultiValueMap<>();
+        valueMap.add("file", new ClassPathResource("51206152.jpg"));
+
+        HttpEntity<MultiValueMap<String, Object>> httpEntity = new HttpEntity<>(valueMap, headers);
+
+        ResponseEntity<Boolean> response = restTemplate.postForEntity("http://localhost:8080/upload", httpEntity, Boolean.class);
+
+        LOGGER.info("The response is {}", response.getBody());
+    }
+
+    @Test
+    void testDownload() throws IOException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_OCTET_STREAM));
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<byte[]> response = restTemplate.exchange("http://localhost:8080/download/51206152.jpg", HttpMethod.GET, entity, byte[].class);
+
+        //We need to convert the byte array into the image back
+        Files.write(Path.of(directory+"download\\51206152.jpg"), response.getBody());
+
     }
 
 }
